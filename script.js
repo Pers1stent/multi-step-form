527f860e4b4d903d2a882ce90c5f725f33034806
Pers1stent <maksbellof@gmail.com>
2023-11-25 14:45:41 +0000

    new features

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const username = document.getElementById("user_name");
 const tel = document.getElementById("user_tel");
 const errorMessage = document.querySelectorAll(".error_message");
 const inputForm = document.querySelectorAll(".input_block");
+const checkbox = document.querySelector(".checkbox");
+const change = document.querySelectorAll(".underline");
 
-// const cards = document.querySelector(".cards");
 const card = document.querySelectorAll(".card");
 const card_monthly = document.querySelectorAll(".card_monthly");
 const cardsYear = document.querySelector(".cards");
@@ -22,10 +23,12 @@ const divTel = document.querySelector(".tel");
 
 const nextButton1 = document.querySelector(".step_1");
 const nextButton2 = document.querySelector(".step_2");
-const nextButton3 = document.querySelector(".step_3");
+const nextButton3 = document.querySelectorAll(".step_3");
+const confirmButton = document.querySelectorAll(".confirm");
 
 const prevButton1 = document.querySelector(".prev_step");
-const prevButton2 = document.querySelector(".prev_step_3");
+const prevButton2 = document.querySelectorAll(".prev_step_3");
+const prevButton3 = document.querySelectorAll(".prev_step_4");
 
 const number1 = document.querySelector(".number1");
 const number2 = document.querySelector(".number2");
@@ -34,10 +37,13 @@ const number4 = document.querySelector(".number4");
 
 const step1 = document.querySelector(".right_part_step1");
 const step2 = document.querySelector(".right_part_step2");
-const step3 = document.querySelector(".right_part_step3");
-let isValid = true;
+const monthlyStep3 = document.querySelector(".right_part_step3_monthly");
+const yearlyStep3 = document.querySelector(".right_part_step3_yearly");
+const yearlyStep4 = document.querySelector(".right_part_step4_yearly");
+const monthlyStep4 = document.querySelector(".right_part_step4_monthly");
+const confirmation = document.querySelector(".right_part_confirmation");
 
-const checkbox = document.querySelector(".checkbox");
+let isValid = true;
 
 const emailValidation = (email) => {
   const regex = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
@@ -148,19 +154,30 @@ checkbox.addEventListener("change", function () {
 
 nextButton2.addEventListener("click", function () {
   step2.classList.add("hidden");
-  step3.classList.remove("hidden");
+
+  if (checkbox.checked) {
+    yearlyStep3.classList.remove("hidden");
+  } else {
+    monthlyStep3.classList.remove("hidden");
+  }
 
   number2.classList.remove("number_active");
   number3.classList.add("number_active");
 });
 
-prevButton2.addEventListener("click", function () {
-  step2.classList.remove("hidden");
-  step3.classList.add("hidden");
+prevButton2.forEach((prev) =>
+  prev.addEventListener("click", function () {
+    if (checkbox.checked) {
+      yearlyStep3.classList.add("hidden");
+    } else {
+      monthlyStep3.classList.add("hidden");
+    }
+    step2.classList.remove("hidden");
 
-  number2.classList.add("number_active");
-  number3.classList.remove("number_active");
-});
+    number2.classList.add("number_active");
+    number3.classList.remove("number_active");
+  })
+);
 
 function toggleCheckbox() {
   const checked = this.querySelector(".add-ons_check");
@@ -172,4 +189,117 @@ function toggleCheckbox() {
 
 addOns.forEach((addOn) => {
   addOn.addEventListener("click", toggleCheckbox);
+});
+
+nextButton3.forEach((next) =>
+  next.addEventListener("click", function () {
+    if (checkbox.checked) {
+      yearlyStep3.classList.add("hidden");
+      yearlyStep4.classList.remove("hidden");
+    } else {
+      monthlyStep3.classList.add("hidden");
+      monthlyStep4.classList.remove("hidden");
+    }
+
+    number3.classList.remove("number_active");
+    number4.classList.add("number_active");
+  })
+);
+
+change.forEach((ch) =>
+  ch.addEventListener("click", function () {
+    monthlyStep4.classList.add("hidden");
+    step2.classList.remove("hidden");
+  })
+);
+
+prevButton3.forEach((prev) =>
+  prev.addEventListener("click", function () {
+    if (checkbox.checked) {
+      yearlyStep4.classList.add("hidden");
+      yearlyStep3.classList.remove("hidden");
+    } else {
+      monthlyStep4.classList.add("hidden");
+      monthlyStep3.classList.remove("hidden");
+    }
+
+    number3.classList.add("number_active");
+    number4.classList.remove("number_active");
+  })
+);
+
+confirmButton.forEach((confirm) =>
+  confirm.addEventListener("click", function () {
+    if (checkbox.checked) {
+      yearlyStep4.classList.add("hidden");
+    } else {
+      monthlyStep4.classList.add("hidden");
+    }
+    confirmation.classList.remove("hidden");
+  })
+);
+
+addOns.forEach((addOn, index) => {
+  addOn.addEventListener("click", function () {
+    const optionContainerMonthly = document.querySelector(
+      ".option_container_monthly"
+    );
+    const optionContainerYearly = document.querySelector(
+      ".option_container_yearly"
+    );
+
+    const optionText = this.querySelector("h3").textContent;
+    const optionPriceText = this.querySelector("p.price").textContent;
+
+    const newOption = document.createElement("div");
+    newOption.classList.add(`option1`);
+    newOption.innerHTML = `
+        <p>${optionText}</p>
+        <p>${optionPriceText}</p>
+      `;
+
+    const newOption1 = document.createElement("div");
+    newOption1.classList.add(`option1`);
+    newOption1.innerHTML = `
+          <p>${optionText}</p>
+          <p>${optionPriceText}</p>
+        `;
+
+    // Toggle clicked class
+    this.classList.toggle("clicked");
+
+    if (this.classList.contains("clicked")) {
+      optionContainerYearly.appendChild(newOption1.cloneNode(true));
+      optionContainerMonthly.appendChild(newOption.cloneNode(true));
+    } else {
+      const optionToRemoveYearly = document.querySelector(
+        `.right_part_step4_yearly .option1`
+      );
+      const optionToRemoveMonthly = document.querySelector(
+        `.right_part_step4_monthly .option1`
+      );
+
+      optionContainerMonthly.removeChild(optionToRemoveMonthly);
+      optionContainerYearly.removeChild(optionToRemoveYearly);
+    }
+
+    updateTotal();
+  });
+
+  function updateTotal() {
+    const optionPrices = document.querySelectorAll(
+      ".option_container > div:not(.option_summary) > p:last-child"
+    );
+    let total = 0;
+    optionPrices.forEach((price) => {
+      total += parseFloat(price.textContent.replace(/[^\d.-]/g, ""));
+    });
+
+    const totalElements = document.querySelectorAll(
+      ".option_summary > p:last-child"
+    );
+    totalElements.forEach((element) => {
+      element.textContent = `$${total}/mo`;
+    });
+  }
 });
